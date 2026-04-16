aa8044bc32b263d6ebd462bf135f5d724538da3f
Frosty <112929809+FrostyHec@users.noreply.github.com>
2026-04-16 17:19:44 +0800

    fix: update User.java formatting and enhance HugeClientBuilderTest with additional checks

--- hugegraph-client/src/main/java/org/apache/hugegraph/structure/auth/User.java
+++ hugegraph-client/src/main/java/org/apache/hugegraph/structure/auth/User.java
@@ -155,10 +155,10 @@ public class User extends AuthElement {
 
         // Mapping of: graphSpace -> graph -> permission -> resourceType -> resources
         @JsonProperty("roles")
-        private Map<String, Map<String, Map<HugePermission, Map<String, 
+        private Map<String, Map<String, Map<HugePermission, Map<String,
                 List<HugeResource>>>>> roles;
 
-        public Map<String, Map<String, Map<HugePermission, Map<String, 
+        public Map<String, Map<String, Map<HugePermission, Map<String,
                 List<HugeResource>>>>> roles() {
             return Collections.unmodifiableMap(this.roles);
         }
--- hugegraph-client/src/test/java/org/apache/hugegraph/unit/HugeClientBuilderTest.java
+++ hugegraph-client/src/test/java/org/apache/hugegraph/unit/HugeClientBuilderTest.java
@@ -1,8 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.hugegraph.unit;
 
 import org.apache.hugegraph.driver.HugeClient;
 import org.apache.hugegraph.driver.HugeClientBuilder;
-import org.apache.hugegraph.rest.ClientException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,42 +26,56 @@ public class HugeClientBuilderTest {
 
     @Test
     public void testBuilderWithSkipRequiredChecks() {
-        // Should not throw IllegalArgumentException when skipRequiredChecks is true and graph is null
-        HugeClientBuilder builder = new HugeClientBuilder("http://127.0.0.1:8080", "DEFAULT", null, true);
-        try {
-            builder.build();
-        } catch (IllegalArgumentException e) {
-            Assert.fail("Should not throw IllegalArgumentException when skipRequiredChecks is true, but got: " + e.getMessage());
-        } catch (Exception e) {
-            // Expected since there is probably no server running at localhost:8080
-            // The fact we reach here means the bypass of graph/url check was successful.
-        }
+        // Constructor should NOT throw when skipRequiredChecks=true, even with null graph/url.
+        // No build() call here to avoid triggering HTTP (no server available in unit tests).
+        HugeClientBuilder builder = new HugeClientBuilder(
+                "http://127.0.0.1:8080", "DEFAULT", null, true);
+        Assert.assertNotNull(builder);
+        // Also verify null url is accepted when skipRequiredChecks=true
+        HugeClientBuilder builder2 = new HugeClientBuilder(
+                null, "DEFAULT", "hugegraph", true);
+        Assert.assertNotNull(builder2);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testBuilderWithoutSkipRequiredChecks() {
-        // Should throw exception when skipRequiredChecks is false and graph is null
-        HugeClientBuilder builder = new HugeClientBuilder("http://127.0.0.1:8080", "DEFAULT", null, false);
+    public void testConstructorCheckWithoutSkip() {
+        // Constructor should throw when graph is null and skipRequiredChecks=false
+        new HugeClientBuilder("http://127.0.0.1:8080", "DEFAULT", null, false);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBuildCheckWithoutSkip_nullGraph() {
+        // Construct with valid params, null out graph afterwards to test build() check path
+        HugeClientBuilder builder = new HugeClientBuilder(
+                "http://127.0.0.1:8080", "DEFAULT", "hugegraph", false);
+        builder.configGraph(null);
+        builder.build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBuildCheckWithoutSkip_nullUrl() {
+        // Construct with valid params, null out url afterwards to test build() check path
+        HugeClientBuilder builder = new HugeClientBuilder(
+                "http://127.0.0.1:8080", "DEFAULT", "hugegraph", false);
+        builder.configUrl(null);
         builder.build();
     }
 
     @Test
     public void testHugeClientBuilderMethod() {
-        // Should not throw IllegalArgumentException
-        HugeClientBuilder builder = HugeClient.builder("http://127.0.0.1:8080", "DEFAULT", null, true);
-        try {
-            builder.build();
-        } catch (IllegalArgumentException e) {
-            Assert.fail("Should not throw IllegalArgumentException when skipRequiredChecks is true, but got: " + e.getMessage());
-        } catch (Exception e) {
-            // Expected
-        }
+        // HugeClient.builder factory should NOT throw when skipRequiredChecks=true.
+        // No build() call here to avoid triggering HTTP (no server available in unit tests).
+        HugeClientBuilder builder = HugeClient.builder(
+                "http://127.0.0.1:8080", "DEFAULT", null, true);
+        Assert.assertNotNull(builder);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testHugeClientBuilderMethodWithoutSkip() {
-        // Should throw exception
-        HugeClientBuilder builder = HugeClient.builder("http://127.0.0.1:8080", "DEFAULT", null);
+        // build() should throw when graph is null and skipRequiredChecks defaults to false
+        HugeClientBuilder builder = HugeClient.builder(
+                "http://127.0.0.1:8080", "DEFAULT", "hugegraph");
+        builder.configGraph(null);
         builder.build();
     }
 }
