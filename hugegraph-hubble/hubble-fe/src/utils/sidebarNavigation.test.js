eee7b795758f218b212af30af6fa132ea19d62fa
imbajin <jin@apache.org>
2026-07-12 14:05:52 +0800

    fix(hubble): select schema navigation
    - derive sidebar selection from route and product mode
    - highlight the PD schema preparation entry
    - preserve non-PD graph metadata under graph understanding
    - cover route helper and rendered menu selection

---
+++ hugegraph-hubble/hubble-fe/src/utils/sidebarNavigation.test.js
@@ -0,0 +1,35 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import {getSidebarMenuKey} from './sidebarNavigation';
+
+test.each([
+    [true, '/graphspace/SPACE/schema', 'schema'],
+    [true, '/graphspace/SPACE/schema/', 'schema'],
+    [true, '/graphspace/SPACE', 'graphspace'],
+    [false, '/graphspace/DEFAULT/graph/hugegraph/meta', 'graphspace'],
+    [false, '/graphspace/DEFAULT/graph/hugegraph/detail', 'graphspace'],
+    [false, '/navigation', 'navigation'],
+    [false, '/', 'navigation'],
+])('maps mode-aware route %s %s to %s', (pdEnabled, pathname, expected) => {
+    expect(getSidebarMenuKey(pathname, pdEnabled)).toBe(expected);
+});
+
+test('does not advertise the PD-only Schema entry in non-PD mode', () => {
+    expect(getSidebarMenuKey('/graphspace/DEFAULT/schema', false)).toBe('graphspace');
+});
